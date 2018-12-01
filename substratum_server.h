7aede6aa61f5077ca0ccc56c538da38d2a40ad6b
Gaetano <gaetano.denardis3dgmail.com>
2018-12-01 02:26:00 -0800

    Useless commit

--- substratum_server.h
+++ substratum_server.h
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
+//non so cosa deve fare bulabula
 int                 pow_int                             (int a, int exp);
 int                 checked_p_range_input               (char *input_string, int a, int b);
 int                 check_dot_addr                      (char *input, int length);
