0ff12911ba07d9fc39cd1a4947ee7d20d1548639
Gaetano <gaetano.denardis3dgmail.com>
2018-12-05 07:32:59 -0800

    Pulizia commenti

--- server_main.c
+++ server_main.c
@@ -15,7 +15,7 @@
 
 head_list_serv *list_server = NULL;
 
-int     num_no_conn_server = 0;
+
 int     *list_data_from_server = NULL; // per la search; quando viene eseguita, gli altri server devono comunicare
 // la loro coppia di chiave x e che valore ha associato y.
 
@@ -45,16 +45,23 @@ int main(int argc, char *argv[]) {
     if(argc != 3){
         sprintf(err_buf, "Wrong number of args\n");
         write(2,err_buf,strlen(err_buf));
+        free(err_buf);
         exit(-1);
     }
     if((checked_p_range_input(argv[2],0,9999)<0)){
         sprintf(err_buf, "Invalid PORT\n");
         write(2,err_buf,strlen(err_buf));
+        free(err_buf);
         exit(-1);
     }
 
     fd = open(argv[1], O_RDONLY);
-    if(fd<0){sprintf(err_buf,"ERR_OPEN_FILE\n\n");write(2,err_buf,strlen(err_buf));exit(-1);}
+    if(fd<0){
+        sprintf(err_buf,"ERR_OPEN_FILE\n\n");
+        write(2,err_buf,strlen(err_buf));
+        free(err_buf);
+        exit(-1);
+    }
 
 
     while(read(fd, buf+i, 1) == 1) {
@@ -74,7 +81,6 @@ int main(int argc, char *argv[]) {
                 sprintf(err_buf,"ERR_CREATE_THREAD\n");
                 write(2,err_buf,strlen(err_buf));
                 free(err_buf);
-                err_buf = NULL;
                 exit(-1);
             }
 
